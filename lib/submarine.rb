40841153fefda6c1d396b56a2f8081510cff4377
Jose Fernandez <jose@umn.edu>
2008-05-02 18:21:15 -0500

    First working prototype, grabs current subdomain, is smart enough to know when you're running a localhost copy and will use the correct call to request.subdomains

--- lib/submarine.rb
+++ lib/submarine.rb
@@ -1,1 +1,23 @@
-# Submarine
+module Submarine
+  
+  mattr_writer :subdomain_model
+  def self.subdomain_model
+    @@subdomain_model ||= 'user'
+  end
+  
+  mattr_writer :subdomain_column
+  def self.subdomain_column
+    @@subdomain_column ||= 'login'
+  end
+  
+  def self.included(controller)
+    controller.helper_method(:current_subdomain)
+  end
+    
+protected
+
+  def current_subdomain
+    request.host.include?('localhost') ? request.subdomains(0).first : request.subdomains.first
+  end
+  
+end 
