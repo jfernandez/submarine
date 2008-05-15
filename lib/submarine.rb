f446152f380f1b981a5225d1ff41a73022ba046c
Jose Fernandez <jose@umn.edu>
2008-05-15 18:54:31 -0500

    Will now alias the submarine methods to the subdomain model name

--- lib/submarine.rb
+++ lib/submarine.rb
@@ -6,16 +6,36 @@ module Submarine
     @@subdomain_model ||= 'user'
     @@subdomain_column ||= 'login'
     
-    controller.helper_method(:current_subdomain)
+    alias_method "#{@@subdomain_model}_domain", :submarine_domain
+    alias_method "#{@@subdomain_model}_subdomain", :submarine_subdomain
+    alias_method "#{@@subdomain_model}_host", :submarine_host
+    alias_method "#{@@subdomain_model}_url", :submarine_url
+    controller.helper_method("#{@@subdomain_model}_domain", "#{@@subdomain_model}_subdomain", "#{@@subdomain_model}_host", "#{@@subdomain_model}_url")
   end
   
   protected
   
-    def default_subdomain
+    def default_submarine_subdomain
       instance.send(Submarine.subdomain_column) if instance && instance.respond_to?(Submarine.subdomain_column)
     end
+    
+    def submarine_url(submarine_subdomain = default_submarine_subdomain, use_ssl = request.ssl?)
+      (use_ssl ? "https://" : "http://") + submarine_host(submarine_subdomain)
+    end
+    
+    def submarine_host(submarine_subdomain = default_submarine_subdomain)
+      submarine_host = ""
+      submarine_host << submarine_subdomain + "."
+      submarine_host << submarine_domain
+    end
+    
+    def submarine_domain
+      submarine_domain = ""
+      submarine_domain << request.subdomains[1..-1].join(".") + "." if request.subdomains.size > 1
+      submarine_domain << request.domain + request.port_string
+    end
   
-    def current_subdomain
+    def submarine_subdomain
       request.host.include?('localhost') ? request.subdomains(0).first : request.subdomains.first
     end
   
